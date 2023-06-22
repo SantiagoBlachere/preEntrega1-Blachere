08ff690279f9da2f4ffbe207d24fd7ee6a723d95
Santiago Blachere <gotregotregotre@gmail.com>
2023-06-22 14:15:54 -0300

    Crear configuración inicial y componente Navbar

---
+++ preEntrega1-Blachere/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+function Navbar() {
+    return (
+        <div>
+            <h1>Librería Gotria</h1>
+            <img src="" alt="" />
+            <ul>
+                <li>Contacto</li>
+                <li>Instagram</li>
+                <li>Sobre nosotros</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
